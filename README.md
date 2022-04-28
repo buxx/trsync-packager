4d9310a036b8463755208b32c1eee68babd83923
Bastien Sevajol <contact@bux.fr>
2022-04-28 12:43:39 -0700

    compile commands

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 If Windows < 11, install winget. Then :
 
+* Install git `winget install git`
 * Install Python 3.8 `winget install -e --id Python.Python.3 -v 3.8.10150.0`
 * Install build tools `winget install -e --id Microsoft.VisualStudio.2022.BuildTools`
 * Install Rust from https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe
--- make.py
+++ make.py
@@ -1,13 +1,46 @@
 from codecs import ignore_errors
+import pathlib
 import subprocess
 import sys
 
 def exec(command: str) -> None:
-    print(subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True).decode(errors="ignore"))
+    process = subprocess.run(command, stderr=subprocess.PIPE, shell=True)
+    if process.stdout:
+        print(process.stdout.decode(errors="ignore"))
+    if process.stderr:
+        print(process.stderr.decode(errors="ignore"))
+    if process.returncode:
+        exit(process.returncode)
 
 
 def main():
-    exec("dir C:")
+    print("Build trsync ...")
+    if not pathlib.Path("trsync").exists():
+        exec("git clone https://github.com/buxx/trsync")
+    exec("cd trsync && git pull")
+    exec("cd trsync && cargo build --release --features windows")
+    
+    print("Build trsync-manager ...")
+    if not pathlib.Path("trsync-manager").exists():
+        exec("git clone https://github.com/buxx/trsync-manager")
+    exec("cd trsync-manager && git pull")
+    exec("cd trsync-manager && cargo build --release")
+    
+    print("Build trsync-manager-systray ...")
+    if not pathlib.Path("trsync-manager-systray").exists():
+        exec("git clone https://github.com/buxx/trsync-manager-systray")
+    exec("cd trsync-manager-systray && git pull")
+    exec("cd trsync-manager-systray && cargo build --release")
+    
+    print("Build trsync-manager-configure ...")
+    if not pathlib.Path("trsync-manager-configure").exists():
+        exec("git clone https://github.com/buxx/trsync-manager-configure")
+    if not pathlib.Path("trsync-manager-configure\venv").exists():
+        exec("cd trsyn-manager-configurec && python -m venv venv")
+    exec("cd trsyn-manager-configure && git pull")
+    exec("cd trsyn-manager-configure && .\venv\Scripts\Activate.ps1 && pip install pip setuptools wheel pyinstaller")
+    exec("cd trsyn-manager-configure && .\venv\Scripts\Activate.ps1 && pip install -r requirements.txt")
+    exec("cd trsyn-manager-configure && .\venv\Scripts\Activate.ps1 && pyinstaller --onefile run.py")
 
 
 if __name__ == "__main__":
